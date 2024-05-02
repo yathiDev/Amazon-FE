b9059f7cf8c7252efb6d1e0d0b381316332c2af0
ElevenDevOps <119465994+ElevenDevOps@users.noreply.github.com>
2024-05-02 14:53:51 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "infra-terraform-bucket-v1"  # Replace with your actual S3 bucket name
+    bucket = "ravi-project-s3bucket"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "ap-south-1" 
   }
