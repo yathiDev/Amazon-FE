c6842ccadd8f5be6fbc44d76edb95b6b7f275cc5
yathiDev <158311781+yathiDev@users.noreply.github.com>
2024-05-07 01:56:44 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-eks-cluster-s3-bucket-02"  # Replace with your actual S3 bucket name
+    bucket = "my-mega123"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "ap-southeast-1" 
   }
